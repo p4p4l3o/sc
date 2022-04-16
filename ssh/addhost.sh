2de414381d692879a9c09feca1fb2e5f56fb8ab0
p4p4l3o <98163388+p4p4l3o@users.noreply.github.com>
2022-04-16 19:51:09 +0800

    Add files via upload

---
+++ ssh/addhost.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- icanhazip.com);
+IZIN=$( curl https://p4p4l3o.github.io/izin | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${CYAN}Akses Diizinkan...${off}"
+sleep 1
+else
+clear
+echo -e "${PURPLE}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+clear
+echo "Checking VPS"
+clear
+read -rp "Domain/Host : " -e domain
+echo "IP=$domain" >>/var/lib/akbarstorevpn/ipvps.conf
+rm -rf /etc/xray/domain
+echo $domain > /etc/xray/domain
+certv2ray
