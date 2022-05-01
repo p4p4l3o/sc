e6d922ad0161ecd0a4b784402e25bfcfbe288553
p4p4l3o <98163388+p4p4l3o@users.noreply.github.com>
2022-05-01 23:39:25 +0800

    Add files via upload

---
+++ xray/certv2ray(2).sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+# Getting
+clear
+echo start
+sleep 0.5
+source /var/lib/premium-script/ipvps.conf
+domain=$IP
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
+wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
+bash acme.sh --register-account -m patajunit@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
+bash acme.sh --installcert -d $domain
+--fullchainpath /etc/v2ray/v2ray.crt
+--keypath /etc/v2ray/v2ray.key
+clear
+echo start
+sleep 0.5
+source /var/lib/premium-script/ipvps.conf
+domain=$IP
+systemctl stop v2ray
+systemctl stop v2ray@none
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+systemctl start v2ray
+systemctl start v2ray@none
+echo Done
+echo " Reboot 5 Sec"
+sleep 5
+reboot
